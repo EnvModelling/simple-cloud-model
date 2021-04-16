8d10c9aec3072a46888c779fa7e248db2f7dea41
Paul Connolly <p.connolly@manchester.ac.uk>
2021-04-16 10:07:09 +0100

    temporary plumbing

--- driver_code.f90
+++ driver_code.f90
@@ -213,7 +213,7 @@
 			    kp,o_halo,dt,dz2,dz2,q,precip,theta,p, &
 				z,theta_ref,rhoa,rho,u,micro_init,hm_flag,mass_ice,ice_flag,&
 				wr_flag,rm_flag, theta_flag, &
-				0.0_sp,1)
+				0.0_sp,1,0,.false.)
 			! calculate precipitation diagnostics
 		endif       
 
